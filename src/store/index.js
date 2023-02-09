3cce09b5c8e3c6bbddd3755c682b9aa9cff8c411
Jake Chien <jake_chien@outlook.com>
2023-02-09 13:37:13 -0300

    chore: pinia installed

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,7 @@
-import { createStore } from 'vuex'
+import { defineStore } from "pinia"
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+export default defineStore({
+  state: () => ({}),
+  getters: {},
+  actions: {},
 })
