efc17ae214e07b467373b985fc1f41882bea7ced
Jake Chien <jake_chien@outlook.com>
2023-02-10 20:45:45 -0300

    style: add style to pokemon cards

--- src/components/PokemonCards.vue
+++ src/components/PokemonCards.vue
@@ -1,6 +1,6 @@
 <template>
   <article v-if="pokemonEvolution">
-    <div>
+    <div class="outside-container">
       <router-link :to="{ name: 'pokemon-details', params: { name: pokemonDetails.name } }">
         <div class="pokemon-card">
           <p>
@@ -10,7 +10,7 @@
         </div>
       </router-link>
     </div>
-    <div v-if="firstPokemonEvolved">
+    <div v-if="firstPokemonEvolved" class="outside-container">
       <router-link :to="{ name: 'pokemon-details', params: { name: firstPokemonEvolved.name } }">
         <div>
           <p>
@@ -20,7 +20,7 @@
         </div>
       </router-link>
     </div>
-    <div v-if="secondPokemonEvolved">
+    <div v-if="secondPokemonEvolved" class="outside-container">
       <router-link :to="{ name: 'pokemon-details', params: { name: secondPokemonEvolved.name } }">
         <div>
           <p>
@@ -48,3 +48,42 @@ const {
 } = storeToRefs(pokemonStore)
 
 </script>
+
+<style lang="scss" scoped>
+article {
+  display: flex;
+  justify-content: space-evenly;
+  width: 70vw;
+  flex-wrap: wrap;
+  row-gap: 30px;
+  margin: 50px auto 0;
+
+  img {
+    width: 7em;
+  }
+
+  a {
+    text-decoration: none;
+    color: rgb(63, 31, 31);
+    font-size: 18px;
+
+    p {
+      margin: 1em;
+      font-weight: 700;
+    }
+  }
+
+  .outside-container {
+    border-radius: 10px;
+    background-color: rgb(205, 162, 230);
+    width: 10em;
+    transition: transform 0.4s ease;
+    box-shadow: 6px 6px 30px rgb(44, 44, 44);
+  }
+
+  .outside-container:hover {
+    transform: scale(1.2);
+    transition: 0.5s ease;
+  }
+}
+</style>
